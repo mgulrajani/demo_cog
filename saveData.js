fb284ece55d65b53225adad0456cefa50d0e1db2
mgulrajani <muskaangulrajani@hotmail.com>
2022-11-29 16:36:50 +0530

    skip links

--- saveData.js
+++ saveData.js
@@ -1,8 +1,14 @@
 var storage = window.localStorage;
 function save(){
+    //first form of the html document
 form1=document.forms[0];
+//in the form1 -element username , that ref i am storing in username
 username = form1.elements["username"]; 
+
+//username.value is assigned to user
 user=username.value;
+
+//that is being set in the session storage as avalue of key 'user'
 storage.setItem('user',user);
 
 console.log(user);}
