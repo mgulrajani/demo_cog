ee7538fc87f978578cc9d35de89f735e84658ca3
mgulrajani <muskaangulrajani@hotmail.com>
2022-12-13 15:33:41 +0530

    array changes

--- javascriptdemos/ch8 Arrays/app.js
+++ javascriptdemos/ch8 Arrays/app.js
@@ -1,4 +1,55 @@
-const numbers=[1,2,3,4,5];
+const numbers=[1,2,3,4,4,5,6,7,5,2,1,5];
+console.log(numbers.fill(1,5,9));
+//index and value --entry -- entries
+//[index-value]
+const entries =  numbers.entries();
+console.log('iterating thru the entries of numbers array')
+for ( const [index,element] of numbers.entries()){
+
+    console.log(index,element);
+}
+
+
+const names=['hetal','nidhi','sagar','hiten','karan'];
+console.log(names.join('**'));
+
+console.log(names.includes('karan'));
+console.log(numbers.toString());
+const keys =  numbers.keys();
+
+for(const key of keys){
+    console.log(key);
+}
+//lastIndexOf()  -- last index of element 
+
+//add at the 10th location
+numbers[9]=100;
+console.log(numbers.length);
+
+console.log(numbers);
+
+let mappedNumbers = numbers.map(e=>e*2);
+
+
+console.log(mappedNumbers);
+console.log('using reducer to find the sum of the array');
+const initialValue=0;
+const sumtotal = numbers.reduce((sum,val)=>sum+=val);
+console.log(sumtotal);
+
+
+//reduceRight method -- applies a function on accumulator and each
+//value of the array from right to left reduces to a single value
+
+const arr1 = [[0,1],[2,3],[4,5]];
+
+const result =arr1.reduce((accumulator , currentValue)=>
+
+    accumulator.concat(currentValue)
+
+);
+console.log('Printing concatenated result using reduce Right method ofarray')
+console.log(result);
 const nums2 = new Array(34,45,56,45,67,88);
 const fruits= ["Apples","Pears","Guavas","Oranges"];
 const mixedArray = [null,true,121,344,"hi",new Date()];
@@ -21,12 +72,12 @@ console.log(numbers);
 numbers.unshift(250);
 console.log(numbers);
 //take off from the end
-console.log(numbers.pop());
+console.log('last element'+numbers.pop());
 console.log(numbers)
 
 
 //take off from the beginning of the array
-console.log(numbers.shift());
+console.log('first element'+ numbers.shift());
 console.log(numbers.reverse());
 console.log(numbers.splice(1,2));
 console.log(numbers);
@@ -40,7 +91,7 @@ console.log(val.sort(function(x,y){
 //iterating through the numbers array ,using forEach , every element is displayed
 numbers.forEach(e=>{
     console.log(e);
-
+    
 });
 
 numbers[3]=90;
---
+++ javascriptdemos/ch8 Arrays/app2.js
@@ -0,0 +1,13 @@
+const fruits = [];
+fruits.push("banana", "apple", "peach");
+console.log(fruits.length);
+fruits[5] = "mango";
+console.log(fruits[5]); // 'mango'
+console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
+console.log(fruits.length);
+
+fruits.length = 10;
+console.log(fruits); // ['banana', 'apple', 'peach', empty x 2, 'mango', empty x 4]
+console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
+console.log(fruits.length); // 10
+console.log(fruits[8]); 
