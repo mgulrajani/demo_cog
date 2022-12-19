fc88987fa2ab147530a23199b2a1f926e26cd99e
mgulrajani <muskaangulrajani@hotmail.com>
2022-12-19 13:04:17 +0530

    assignment2

--- javascriptdemos/fetchdemo/studentinfo.js
+++ javascriptdemos/fetchdemo/studentinfo.js
@@ -6,7 +6,7 @@ class Student{
         this.city =city;
         this.clearedExam=clearedExam;
     }
-}
+}/* 
 async function loadStudents() {
     const response = await fetch('./assets/students.json');
     const students = await response.json();
@@ -22,22 +22,57 @@ async function loadStudents() {
     console.log(map);
 
 }
+ */
+class Map1{
+  constructor(map){this.map=map;}
+  
+  addMarks=(student,mark)=>{
 
-addMarks=(student,...mark)=>{
 
-    if(map.has(student))
-      {
-        
-         map.set(student,[...map.get(student),mark]);
-      }
-      else{
-        map.set(student,[mark]);
-      }
-      return map.get(student);
+    this.map.forEach((v,k)=>{
+        if(k===student){
+  
+        this.map.set(k,[...v,...mark])
 
-};
+        }
+        else{
+            this.map.set(k,v);
+        }
 
-  loadStudents();
 
-  let nisha=new Student(999,'nisha','bangalore',true);
-  console.log(addMarks(nisha,[12,23,34]));
+    })
+
+
+}
+    
+}
+  //loadStudents();
+
+  let tia=new Student(999,'tia','pune',true);
+ 
+  let nidhi=new Student(333,'nidhi','mumbai',true);
+ 
+  let rishi=new Student(6666,'rishi','simla',true);
+ 
+  let nisha=new Student(5555,'nisha','bangalore',true);
+  
+  map.set(nisha,[]);
+  map.set(nidhi,[56,78,88]);
+  map.set(tia,[]);
+  map.set(rishi,[56,78,88]);
+  
+  let map1=new Map1(map);
+
+
+
+  console.log(map1.addMarks(nisha,[12,23,34]));
+
+
+  map1.addMarks(nisha,[67,78,99]);
+  map1.addMarks(rishi,[45,56,74]);
+  map1.addMarks(tia,[50,90,90]);
+  map1.addMarks(nidhi,[12,32,34]);
+  map1.addMarks(rishi,[99]);
+
+
+  console.log(map1.map)
